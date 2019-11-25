d444907479e518054eba1bfc9aa54ab866c8abdc
lidapeng <lidapeng@chinatelecom.cn>
2019-11-25 19:57:16 +0800

    add comment TypeParameterResolver

--- src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
+++ src/main/java/org/apache/ibatis/reflection/TypeParameterResolver.java
@@ -44,6 +44,13 @@ public class TypeParameterResolver {
    * @return The return type of the method as {@link Type}. If it has type parameters in the declaration,<br>
    *         they will be resolved to the actual runtime {@link Type}s.
    */
+
+  /**
+   * 解析入参method的返回值，将返回值中的泛型解析为具体类型
+   *
+   * 举例： srcType是Level1Mapper.class， method是Level0Mapper.class中的一个方法
+   * （其中，Level1Mapper.class是Level0Mapper.class的子接口）
+   */
   public static Type resolveReturnType(Method method, Type srcType) {
     Type returnType = method.getGenericReturnType();
     Class<?> declaringClass = method.getDeclaringClass();
@@ -64,18 +71,23 @@ public class TypeParameterResolver {
     return result;
   }
 
+  // 从[srcType, declaringClass]这个继承结构中，将typeVar解析为具体化的类型
+  // 举例： type是一个方法的返回值，srcType=Level1Mapper.class，declaringClass是Level0Mapper.class
   private static Type resolveType(Type type, Type srcType, Class<?> declaringClass) {
     if (type instanceof TypeVariable) {
       return resolveTypeVar((TypeVariable<?>) type, srcType, declaringClass);
-    } else if (type instanceof ParameterizedType) {
+    } // type是泛型，例如：List<Double>
+    else if (type instanceof ParameterizedType) {
       return resolveParameterizedType((ParameterizedType) type, srcType, declaringClass);
     } else if (type instanceof GenericArrayType) {
       return resolveGenericArrayType((GenericArrayType) type, srcType, declaringClass);
-    } else {
+    } // type是普通clazz
+    else {
       return type;
     }
   }
 
+  // GenericArrayType表示数组，例如：ArrayList<String>[] listArr;
   private static Type resolveGenericArrayType(GenericArrayType genericArrayType, Type srcType, Class<?> declaringClass) {
     Type componentType = genericArrayType.getGenericComponentType();
     Type resolvedComponentType = null;
@@ -93,14 +105,24 @@ public class TypeParameterResolver {
     }
   }
 
+  // 解析入参parameterizedType 中的泛型。
+  // 因为ParameterizedType类型的变量就是带泛型的变量，（可能带多个泛型）
+  // 本函数会将这些泛型解析成具体的类型，再返回用这些解析后的值，重新构造的ParameterizedType类型对象
   private static ParameterizedType resolveParameterizedType(ParameterizedType parameterizedType, Type srcType, Class<?> declaringClass) {
     Class<?> rawType = (Class<?>) parameterizedType.getRawType();
+    // 取parameterizedType内的泛型，例如：List<Double> 得到 Double.class
+    // 由parameterizedType.getActualTypeArguments 得到的值，
+    // 类型可以是TypeVariable，ParameterizedType，WildcardType，Class，可以参考笔记
     Type[] typeArgs = parameterizedType.getActualTypeArguments();
+
+    // 解析出数组typeArgs中每一个元素的具体化类型，来填充args数组
     Type[] args = new Type[typeArgs.length];
     for (int i = 0; i < typeArgs.length; i++) {
       if (typeArgs[i] instanceof TypeVariable) {
+        // 解析出typeArgs[i]的具体化类型
         args[i] = resolveTypeVar((TypeVariable<?>) typeArgs[i], srcType, declaringClass);
       } else if (typeArgs[i] instanceof ParameterizedType) {
+        // 等号右面得到的是ParameterizedTypeImpl类型的对象（也就是最终结果args数组中会有ParameterizedType类型的元素）
         args[i] = resolveParameterizedType((ParameterizedType) typeArgs[i], srcType, declaringClass);
       } else if (typeArgs[i] instanceof WildcardType) {
         args[i] = resolveWildcardType((WildcardType) typeArgs[i], srcType, declaringClass);
@@ -111,12 +133,19 @@ public class TypeParameterResolver {
     return new ParameterizedTypeImpl(rawType, null, args);
   }
 
+  // 解析入参wildcardType中，上下界的泛型
+  // 返回由解析得到的值，构造成的WildcardTypeImpl对象。
   private static Type resolveWildcardType(WildcardType wildcardType, Type srcType, Class<?> declaringClass) {
+    // 解析入参wildcardType，获取下限。
     Type[] lowerBounds = resolveWildcardTypeBounds(wildcardType.getLowerBounds(), srcType, declaringClass);
+    // 解析入参wildcardType，获取上限。例如：? extends String， 得到String.class
     Type[] upperBounds = resolveWildcardTypeBounds(wildcardType.getUpperBounds(), srcType, declaringClass);
+    // 包装成自定义的WildcardTypeImpl类型返回
     return new WildcardTypeImpl(lowerBounds, upperBounds);
   }
 
+  // 从[srcType, declaringClass]这个继承结构中，
+  // 解析出 入参bounds数组中 每个元素的具体化类型，返回解析后的数组
   private static Type[] resolveWildcardTypeBounds(Type[] bounds, Type srcType, Class<?> declaringClass) {
     Type[] result = new Type[bounds.length];
     for (int i = 0; i < bounds.length; i++) {
@@ -133,9 +162,22 @@ public class TypeParameterResolver {
     return result;
   }
 
+  /**
+   * 解析入参typeVar的具体化类型（就是typeVar是个泛型，需要得到它对应的具体化的类型。 例如：typeVar代表T，若代码中给 T的具体类型是String，则本函数返回String）
+   * 从[srcType, declaringClass]这个继承结构中，查找typeVar的具体化类型
+   *
+   * 具体步骤：
+   * 1.如果srcType的Class类型和declaringClass为同一个类，如果typeVar有上限，则返回typeVar的第一个上界， 否则返回Object.class
+   * 2.如果不是，则代表declaringClass是srcType的父类或者实现的接口，则解析继承结构中有没有定义其具体化的类型
+   *其中的继承结构，包括从srcType到declaringClass的闭合区间
+   *
+   * typeVar是一个方法的返回值，srcType=Level1Mapper.class，declaringClass是Level0Mapper.class
+   */
   private static Type resolveTypeVar(TypeVariable<?> typeVar, Type srcType, Class<?> declaringClass) {
     Type result;
     Class<?> clazz;
+    // 将入参srcType转成Class对象，赋值给clazz。若转不了，则抛出异常。
+    // （可以看出，srcType只能是Class或者ParameterizedType 类型的变量，不然就会抛出异常，即srcType只能是普通类或者带泛型的类）
     if (srcType instanceof Class) {
       clazz = (Class<?>) srcType;
     } else if (srcType instanceof ParameterizedType) {
@@ -145,6 +187,10 @@ public class TypeParameterResolver {
       throw new IllegalArgumentException("The 2nd arg must be Class or ParameterizedType, but was: " + srcType.getClass());
     }
 
+    // 到这 clazz=srcType.class
+
+    // 若clazz等于入参declaringClass，则表示在继承结构中没有获取其具体化的类型
+    // 如果typeVar有上限，则返回typeVar的第一个上界， 否则返回Object.class（这意思是，下面在继承层次中的 所有递归调用，都会在这里终结）
     if (clazz == declaringClass) {
       Type[] bounds = typeVar.getBounds();
       if (bounds.length > 0) {
@@ -154,45 +200,74 @@ public class TypeParameterResolver {
     }
 
     Type superclass = clazz.getGenericSuperclass();
-    result = scanSuperTypes(typeVar, srcType, declaringClass, clazz, superclass);
+    // 通过对clazz父类的扫描，获取入参typeVar指代的实际类型
+    result = scanSuperTypes(typeVar, srcType, declaringClass, clazz, superclass); // clazz是srcType.clazz，srcType=Level1Mapper.class，declaringClass是Level0Mapper.class，superclass是clazz的父类
     if (result != null) {
       return result;
     }
 
     Type[] superInterfaces = clazz.getGenericInterfaces();
     for (Type superInterface : superInterfaces) {
-      result = scanSuperTypes(typeVar, srcType, declaringClass, clazz, superInterface);
+      // 通过对clazz父接口的扫描，获取入参typeVar指代的实际类型
+      result = scanSuperTypes(typeVar, srcType, declaringClass, clazz, superInterface); // clazz是srcType.clazz，srcType=Level1Mapper.class，declaringClass是Level0Mapper.class，superclass是clazz的父接口
       if (result != null) {
         return result;
       }
     }
+    // 如果父类或者父接口中都没有获取到typeVar的具体化类型，则返回Object.class
     return Object.class;
   }
 
+  /**
+   *  通过对父类/接口的扫描获取 入参typeVar指代的实际类型
+   *  typeVar是方法返回值，srcType=Level1Mapper.class，declaringClass是Level0Mapper.class，clazz是srcType.clazz，superclass是clazz的父接口，
+   */
   private static Type scanSuperTypes(TypeVariable<?> typeVar, Type srcType, Class<?> declaringClass, Class<?> clazz, Type superclass) {
+    // 若superclass带泛型，则进if
     if (superclass instanceof ParameterizedType) {
       ParameterizedType parentAsType = (ParameterizedType) superclass;
+      // 获取当前对象的clazz
       Class<?> parentAsClass = (Class<?>) parentAsType.getRawType();
+      // 获取泛型信息
       TypeVariable<?>[] parentTypeVars = parentAsClass.getTypeParameters();
+      // 若子类带泛型
       if (srcType instanceof ParameterizedType) {
+        // 使用子类的具体化泛型，具体化一下父类的泛型，并记录在返回的ParameterizedType对象中 （感觉这句调用，才是解析入参typeVar的重点）
         parentAsType = translateParentTypeVars((ParameterizedType) srcType, clazz, parentAsType);
       }
+      // 如果declaringClass和parentAsClass表示同一类型，
+      // 则先判断出typeVar在parentAsClass的泛型中的位置，再从supperClass中取typeVar对应的具体化的类型，并返回
       if (declaringClass == parentAsClass) {
         for (int i = 0; i < parentTypeVars.length; i++) {
           if (typeVar == parentTypeVars[i]) {
+            // 返回该位置，父类的泛型的具体类型。
+            //（子类继承父类的时候，可能会具体化父类的泛型，这里取的就是父类的泛型被具体化后的类型）
             return parentAsType.getActualTypeArguments()[i];
           }
         }
       }
+      //通过判断parentAsClass是否是declaringClass的子类，来就决定是否进行递归解析（因为当前函数被调用处的java类可以实现多个接口）
       if (declaringClass.isAssignableFrom(parentAsClass)) {
         return resolveTypeVar(typeVar, parentAsType, declaringClass);
       }
-    } else if (superclass instanceof Class && declaringClass.isAssignableFrom((Class<?>) superclass)) {
+    } // superclass是Class类型，还需要判断superclass是否是declaringClass的子类，再决定是否进行递归解析。（因为当前函数被调用处的java类可以实现多个接口）
+    else if (superclass instanceof Class && declaringClass.isAssignableFrom((Class<?>) superclass)) {
       return resolveTypeVar(typeVar, superclass, declaringClass);
     }
     return null;
   }
 
+
+  /**
+   * 使用子类的具体化的泛型信息，将父类提供的泛型给标识出具体类型。
+   * 返回一个ParameterizedType对象，包含父类的这些具体化的泛型信息。
+   *
+   *
+   * @param srcType 举例： Level1Mapper<E, F>
+   * @param srcClass 是srcType.clazz
+   * @param parentType 是srcClass的父类/接口
+   * @return
+   */
   private static ParameterizedType translateParentTypeVars(ParameterizedType srcType, Class<?> srcClass, ParameterizedType parentType) {
     Type[] parentTypeArgs = parentType.getActualTypeArguments();
     Type[] srcTypeArgs = srcType.getActualTypeArguments();
@@ -200,17 +275,24 @@ public class TypeParameterResolver {
     Type[] newParentArgs = new Type[parentTypeArgs.length];
     boolean noChange = true;
     for (int i = 0; i < parentTypeArgs.length; i++) {
+      // parentTypeArgs[i]是泛型，则尝试用子类的具体化泛型去辨别parentTypeArgs[i]的具体类型
       if (parentTypeArgs[i] instanceof TypeVariable) {
         for (int j = 0; j < srcTypeVars.length; j++) {
+          // 子类的泛型中，有一个和父类相同（这表示子类在继承父类时，没有具体化该位置的泛型）
+          // 类似： public interface Level1Mapper<E, F> extends Level0Mapper<E, F, String> {} 中的E和F。 子类中的E和父类的E是同一个对象
+          // 这段代码的意思应该是 相等表示父类泛型还没有被具体化，就取子类对泛型的具体化的类型。
           if (srcTypeVars[j] == parentTypeArgs[i]) {
             noChange = false;
+            // 取子类对泛型的具体化的类型。
             newParentArgs[i] = srcTypeArgs[j];
           }
         }
-      } else {
+      } // parentTypeArgs[i]已经具体化了，直接保存到newParentArgs
+      else {
         newParentArgs[i] = parentTypeArgs[i];
       }
     }
+    // 对父类重新构造一个ParameterizedType对象返回
     return noChange ? parentType : new ParameterizedTypeImpl((Class<?>)parentType.getRawType(), null, newParentArgs);
   }
 
